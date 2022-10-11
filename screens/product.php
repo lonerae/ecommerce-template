2782993ab845443924fa96472a91ccbc2d6b7506
lonerae <78692050+lonerae@users.noreply.github.com>
2022-10-11 17:41:10 +0300

    Update product.php

--- screens/product.php
+++ screens/product.php
@@ -17,8 +17,8 @@
 <body class="container-fluid">
 
 <!--Navigation Bar-->
-<div id="nav-placeholder"></div>
-
+<?php include "standalone/navbar.php";?>
+  
 <?php
     $con = connect('db_template');
 
@@ -149,19 +149,10 @@
 } ?>
 
 <!--Footer-->
-<div id="footer-placeholder"></div>
-
+<?php include "standalone/footer.html";?>
+  
 </body>
 </html>
-
-<script>
-$(function() {
-  $("#nav-placeholder").load("standalone/navbar.php");
-});
-$(function() {
-  $("#footer-placeholder").load("standalone/footer.html");
-})
-</script>
 
 <script>
       document.getElementById("selector").onchange = changeListener;
