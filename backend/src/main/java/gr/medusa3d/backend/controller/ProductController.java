c4949d41cdeaf2b381b8726916f6e040aa0a5527
lonerae <78692050+lonerae@users.noreply.github.com>
2023-01-22 13:11:47 +0200

    allowed frontend to connect to custom api

--- backend/src/main/java/gr/medusa3d/backend/controller/ProductController.java
+++ backend/src/main/java/gr/medusa3d/backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package gr.medusa3d.backend.controller;
 import gr.medusa3d.backend.model.Product;
 import gr.medusa3d.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path="api/product")
 public class ProductController {
 
